2dcfeb20a582ce38b84ca335bf85dcebd778f958
Jian Chen <jian@Jians-MacBook-Pro.local>
2019-06-22 00:05:02 -0700

    remove redundant file

--- ModelUploader.py
+++ ModelUploader.py
@@ -1,13 +1,13 @@
 from azure.storage import CloudStorageAccount
 
-class AzureUploader():
+class ModelUploader():
     def __init__(self):
         account_name = 'jianalphazerostorage'
-        account_key = #''
+        account_key = ''
         account = CloudStorageAccount(account_name, account_key)
         self.blockblob_service = account.create_block_blob_service()
         self.container_name = 'alphamodel'
-        
+
     def upload(self, modelPath, modelName):
         self.blockblob_service.create_blob_from_path(self.container_name, modelName, modelPath)
         generator = self.blockblob_service.list_blobs(self.container_name)
